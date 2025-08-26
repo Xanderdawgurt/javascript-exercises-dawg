5220154005f840b3342431298c40096dda37d6f1
Xander <xdancel06@gmail.com>
2025-08-25 20:37:08 -0500

    finish problem 06 repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let i = 0; 
+    let string = ""
+    while (i < num) {
+        string += str; 
+        i++;
+    }
+    return string;
 };
+
+console.log(repeatString('bobs', 60));
 
 // Do not edit below this line
 module.exports = repeatString;

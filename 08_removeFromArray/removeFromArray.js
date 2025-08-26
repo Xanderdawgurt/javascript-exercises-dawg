5ba4d910e22b1f7c25e6ac492950cb36508ad4bc
Xander <xdancel06@gmail.com>
2025-08-26 18:46:14 -0500

    finish problem 08 removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    const index = arr.indexOf(num);
+    const rm = arr.splice(index, 1);
+    return arr; 
 };
 
 // Do not edit below this line

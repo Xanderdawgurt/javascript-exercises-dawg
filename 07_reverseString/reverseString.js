cc25452188f7c4455c3877fa3294d3781d10b224
Xander <xdancel06@gmail.com>
2025-08-25 21:03:09 -0500

    finish problem 07 reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
 
+
+const reverseString = function(word) {
+    const arr = word.split('');
+    const reversedArr = arr.reverse();
+    return reversedArr.join(''); 
 };
-
+console.log(reverseString(blooper));
 // Do not edit below this line
 module.exports = reverseString;

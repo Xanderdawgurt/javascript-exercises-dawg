444e4ffbcf7c2a8468b2d24c1e95986ee3b0afeb
Xander <xdancel06@gmail.com>
2025-08-27 21:43:11 -0500

    finish problem 07

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  const realNumber = (fah - 32) * (5/9); 
+  const roundedNumber = Math.round(realNumber * 10) / 10; 
+  return roundedNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const realNumber = (cel * 1.8) + 32;
+  const roundedNumber = Math.round(realNumber * 10) / 10;
+  return roundedNumber; 
 };
 
 // Do not edit below this line

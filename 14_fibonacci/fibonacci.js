f6b3c6eea1a295e23ccd4512fdc933ae61340fdb
Xander <xdancel06@gmail.com>
2025-09-21 20:34:50 -0500

    Finish problem 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(z) {
 
+    z = Number(z); 
+
+    if(z < 0) {
+        return "OOPS"; 
+    } else if(z === 0) {
+        return 0; 
+    } else if(z === 1) {
+        return 1; 
+    }
+    
+    let a = 0; 
+    let b = 1; 
+    let c = 0; 
+
+    for(let i = 1; i < z; i++) {
+    
+        c = a + b; 
+
+        a = b; 
+        b = c; 
+    }
+    return c;
 };
 
 // Do not edit below this line
